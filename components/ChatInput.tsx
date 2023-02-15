1fa9ab02bf13ccedd7712508654c5f0e7ae3bb9b
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-15 18:43:58 +0545

    Chat Input field Created with submit message function to submit message to backend

---
+++ components/ChatInput.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { db } from "../firebase";
+
+type Props = {
+  chatId: string;
+};
+
+const ChatInput = ({ chatId }: Props) => {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
+  //TODO: get model input
+  const model = "text-davinci-003";
+
+  const sendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+    const input = prompt.trim();
+    setPrompt("");
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name={$session?.user?.name}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+
+    await fetch("/api/askQuestions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    });
+  };
+  return (
+    <form
+      className="m-4 flex items-center rounded-lg bg-secondary"
+      onSubmit={sendMessage}
+    >
+      <input
+        type="text"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        disabled={!session}
+        placeholder="Enter your message here"
+        className="w-full border-none bg-transparent p-3 text-white outline-none placeholder:text-[14px]"
+      />
+      <button type="submit" disabled={!prompt || !session}>
+        <PaperAirplaneIcon className="mr-4 h-6 w-6 cursor-pointer text-white/40" />
+      </button>
+    </form>
+  );
+};
+
+export default ChatInput;
