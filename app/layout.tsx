8b8f68415f7fe4ec95bbd50ad81fef187dca5d82
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-15 17:17:21 +0545

    Using Session Provider component that I had created Before

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,37 @@
 import { Poppins } from "@next/font/google";
 import "../styles/globals.css";
-import { Sidebar } from "../components";
+import { getServerSession } from "next-auth";
+import { Login, SessionProvider, Sidebar } from "../components";
+import { authOptions } from "../pages/api/auth/[...nextauth]";
 
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang="en" className={`${poppins.className} bg-primary`}>
       <head />
       <body>
-        <div className="flex">
-          {/* <div>
-            <Sidebar />
-          </div> */}
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div>
+                <Sidebar />
+              </div>
 
-          <div className="flex-1">{children}</div>
-        </div>
+              <div className="flex-1">{children}</div>
+            </div>
+          )}
+        </SessionProvider>
       </body>
     </html>
   );
