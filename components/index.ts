b96ea3cabc3faf4ea286c74aeab2d5d873a44189
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-15 17:19:04 +0545

    Component Easy file export

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,4 @@
 export { default as Sidebar } from "./Sidebar";
+export { default as NewChat } from "./NewChat";
+export { default as SessionProvider } from "./SessionProvider";
+export { default as Login } from "./Login";
