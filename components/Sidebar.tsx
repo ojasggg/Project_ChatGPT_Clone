d1732058bdcc15752afcb1b139e6690f9421f47c
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-15 18:03:22 +0545

    While Getting data show data by CreatedBy ascending order

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,17 +1,34 @@
 "use client";
-import { sign } from "crypto";
+
+import { useCollection } from "react-firebase-hooks/firestore";
 import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "../firebase";
+import ChatRow from "./ChatRow";
 
 const Sidebar = () => {
   const { data: session } = useSession();
+  const [chats, loading, error] = useCollection(
+    session &&
+      query(
+        collection(db, "users", session?.user?.email!, "chats"),
+        orderBy("createdAt", "asc")
+      )
+  );
   return (
     <div className="flex h-screen w-[130px] flex-col bg-sidebar_primary text-white  lg:w-[300px]">
-      <div className="m-2 flex-1">
+      <div className="m-2 flex flex-1 flex-col space-y-2">
         <NewChat />
+
+        <div></div>
+        {chats?.docs.map((chat, index) => (
+          <ChatRow key={chat.id} id={chat.id} />
+        ))}
       </div>
+
       <div
-        className="justify-center mb-10 flex cursor-pointer items-center space-x-2"
+        className="mb-10 flex cursor-pointer items-center justify-center space-x-2"
         onClick={() => signOut()}
       >
         {session && (
