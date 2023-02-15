ccc106ef6c3a886b75d4093222735e3277ac1e42
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-16 00:46:05 +0545

    Model Selection Select jsx component is created using react-select and api call to openai to get all models list

---
+++ components/ModelSelection.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from "react";
+import useSWR from "swr";
+import Select from "react-select";
+
+const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
+
+const ModelSelection = () => {
+  const { data: models, isLoading } = useSWR("models", fetchModels);
+
+  const { data: model, mutate: setModel } = useSWR("model", {
+    fallbackData: "text-davinci-003",
+  });
+  return (
+    <div>
+      <Select
+        className="mt-2 text-[#000]"
+        options={models?.modelOptions}
+        defaultValue={model}
+        placeholder={model}
+        isSearchable
+        isLoading={isLoading}
+        menuPosition="fixed"
+        classNames={{
+          control: (state) => "bg-[#434654] border-[#434654]",
+        }}
+        onChange={(e) => setModel(e.Model)}
+      />
+    </div>
+  );
+};
+
+export default ModelSelection;
