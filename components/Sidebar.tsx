87dfef294304871508dcaf3a4ced1fae87ca22bb
Ojash Gurung <OJASGGG@GMAIL.COM>
2023-02-15 17:18:52 +0545

    Logout Functionality with google image data that we got from session data

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,27 @@
-import { PlusIcon } from "@heroicons/react/24/solid";
+"use client";
+import { sign } from "crypto";
+import { useSession, signOut } from "next-auth/react";
 import NewChat from "./NewChat";
 
 const Sidebar = () => {
+  const { data: session } = useSession();
   return (
     <div className="flex h-screen w-[130px] flex-col bg-sidebar_primary text-white  lg:w-[300px]">
       <div className="m-2 flex-1">
         <NewChat />
+      </div>
+      <div
+        className="justify-center mb-10 flex cursor-pointer items-center space-x-2"
+        onClick={() => signOut()}
+      >
+        {session && (
+          <img
+            src={session.user?.image!}
+            className="w-10 cursor-pointer rounded-full"
+            alt="User Image"
+          />
+        )}
+        <p className="text-[14px] text-white/60 lg:text-[16px] ">Logout</p>
       </div>
     </div>
   );
